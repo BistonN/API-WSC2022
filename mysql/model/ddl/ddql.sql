b051d2673e09c52ce83d2d4d9dcf96e293aa2bd2
BistonN <biston.nunes@gmail.com>
2022-10-19 17:06:14 -0300

    arrumando estrutura banco de dados

--- mysql/model/ddl/ddql.sql
+++ mysql/model/ddl/ddql.sql
@@ -1,62 +1,73 @@
 -- -----------------------------------------------------
 -- Schema wsdb
 -- -----------------------------------------------------
-DROP DATABASE IF EXISTS wsdb;
+DROP SCHEMA IF EXISTS wsdb ;
+
+-- -----------------------------------------------------
+-- Schema wsdb
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS wsdb DEFAULT CHARACTER SET utf8 ;
 USE wsdb ;
 
 -- -----------------------------------------------------
--- Table wsdb.users
+-- Table wsdb.usuarios
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS wsdb.users (
-  id_user INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(200) NOT NULL,
+DROP TABLE IF EXISTS wsdb.usuarios ;
+
+CREATE TABLE IF NOT EXISTS wsdb.usuarios (
+  id_usuario INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(200) NOT NULL,
   email VARCHAR(200) NOT NULL,
-  password VARCHAR(200) NOT NULL,
-  wscoins INT NOT NULL DEFAULT 0,
-  created DATETIME NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (id_user))
+  senha VARCHAR(200) NOT NULL,
+  saldo FLOAT NOT NULL DEFAULT 0,
+  dt_criacao DATETIME NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (id_usuario))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table wsdb.products
+-- Table wsdb.produtos
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS wsdb.products (
-  id_product INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) NOT NULL,
-  description VARCHAR(200) NOT NULL,
+DROP TABLE IF EXISTS wsdb.produtos ;
+
+CREATE TABLE IF NOT EXISTS wsdb.produtos (
+  id_produto INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  descricao MEDIUMTEXT NOT NULL,
   img_url VARCHAR(200) NOT NULL,
-  created DATETIME NOT NULL DEFAULT current_timestamp(),
-  id_user_created INT NOT NULL,
-  PRIMARY KEY (id_product),
-  INDEX fk_products_users1_idx (id_user_created ASC) ,
+  preco FLOAT NOT NULL,
+  id_usuario_created INT NOT NULL,
+  dt_criacao DATETIME NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (id_produto),
+  INDEX fk_products_users1_idx (id_usuario_created ASC) VISIBLE,
   CONSTRAINT fk_products_users1
-    FOREIGN KEY (id_user_created)
-    REFERENCES wsdb.users (id_user)
+    FOREIGN KEY (id_usuario_created)
+    REFERENCES wsdb.usuarios (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table wsdb.purchases_historic
+-- Table wsdb.historico_compra
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS wsdb.purchases_historic (
-  purchase_history INT NOT NULL AUTO_INCREMENT,
-  id_user INT NOT NULL,
-  id_product INT NOT NULL,
-  PRIMARY KEY (purchase_history),
-  INDEX fk_purchases_historic_users_idx (id_user ASC) ,
-  INDEX fk_purchases_historic_products1_idx (id_product ASC) ,
+DROP TABLE IF EXISTS wsdb.historico_compra ;
+
+CREATE TABLE IF NOT EXISTS wsdb.historico_compra (
+  id_historico_compra INT NOT NULL AUTO_INCREMENT,
+  id_usuario INT NOT NULL,
+  id_produto INT NOT NULL,
+  PRIMARY KEY (id_historico_compra),
+  INDEX fk_purchases_historic_users_idx (id_usuario ASC) VISIBLE,
+  INDEX fk_purchases_historic_products1_idx (id_produto ASC) VISIBLE,
   CONSTRAINT fk_purchases_historic_users
-    FOREIGN KEY (id_user)
-    REFERENCES wsdb.users (id_user)
+    FOREIGN KEY (id_usuario)
+    REFERENCES wsdb.usuarios (id_usuario)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_purchases_historic_products1
-    FOREIGN KEY (id_product)
-    REFERENCES wsdb.products (id_product)
+    FOREIGN KEY (id_produto)
+    REFERENCES wsdb.produtos (id_produto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
--- mysql/model/ddl/dml.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO users (name, email, password, wscoins)
-VALUES('Amy D. Lankford', 'amydlankford@rhyta.com', '9f804740-be3e-4788-81fd-20dc3dae82e8', 350),
-('Patrick M. Williams', 'patrickmwilliams@jourrapide.com', '95b28556-0f7b-4ab8-ae85-97ac1b53e040', 500);
-
-
